36b48c80a5cc26e45c5ca701a21020903bcf375a
sumukshashidhar <sumukuuu@gmail.com>
2022-07-23 20:34:22 -0500

    feat(env): read time labels file

--- src/read_env_vars.py
+++ src/read_env_vars.py
@@ -1,12 +1,13 @@
 from os import getenv
 from dotenv import load_dotenv
+import json
 
-def get_todoist_api_key():
+def get_todoist_api_key(envname = 'TODOIST_API_KEY'):
     """
     Read the Todoist API key from the environment variables.
 
     Args:
-        None
+        envname: The name of the environment variable.
     
     Returns:
         str: The Todoist API key.
@@ -15,7 +16,20 @@ def get_todoist_api_key():
         Exception: If the Todoist API key is not found.
     """
     load_dotenv() # Loads the .env file into the environment
-    key = getenv('TODOIST_API_KEY')
+    key = getenv(envname)
     if key is None:
-        raise Exception('TODOIST_API_KEY environment variable not set.')
+        raise Exception(f'{envname} not set.')
     return key
+
+def read_time_labels(filename = 'time_label_naming.json'):
+    """
+    Read the time labels from a json file.
+
+    Args:
+        filename: The name of the json file.
+    
+    Returns:
+        dict: A dictionary of labels, with their IDs as keys, and time as values.
+    """
+    with open(filename) as f:
+        return json.load(f)
